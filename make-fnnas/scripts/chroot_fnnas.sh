068ccb656be669016bf2acf6d3de0fd728a2fc2a
ophub <68696949+ophub@users.noreply.github.com>
2026-09-23 09:58:36 +0800

    chroot_fnnas.sh: update the required dependencies

--- make-fnnas/scripts/chroot_fnnas.sh
+++ make-fnnas/scripts/chroot_fnnas.sh
@@ -70,7 +70,7 @@ check_dependencies() {
     export DEBIAN_FRONTEND=noninteractive
 
     # Define necessary packages
-    necessary_packages=("xz-utils" "zstd" "gzip" "zip" "tar" "git" "u-boot-tools" "initramfs-tools" "grub2-common")
+    necessary_packages=("xz-utils" "zstd" "gzip" "zip" "tar" "git" "u-boot-tools" "initramfs-tools" "grub2-common" "parted" "btrfs-progs" "e2fsprogs" "util-linux")
 
     # Fix potential apt lock or list issues in chroot
     mkdir -p /var/lib/apt/lists/partial
